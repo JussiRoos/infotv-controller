83cd69a5103cb667ab9f936e2cd7873efd89b54a
Jens Pitkanen <jens@neon.moe>
2018-03-22 15:39:40 +0200

    Python2 -> 3

--- SETUP.sh
+++ SETUP.sh
@@ -1,3 +1,3 @@
 cp ~/infotv-controller/autostart ~/.config/lxsession/LXDE-pi/autostart
-sudo apt -y install python python-pip scrot xdotool feh
-pip install --user --upgrade google-api-python-client
+sudo apt -y install python3 python3-pip scrot xdotool feh
+pip3 install --user --upgrade google-api-python-client
