8789d1f037e805e742e2ae533ae2067635ec46c9
Jens Pitkanen <jens@neon.moe>
2018-03-22 15:48:13 +0200

    Remove google api dep

--- SETUP.sh
+++ SETUP.sh
@@ -1,3 +1,2 @@
 cp ~/infotv-controller/autostart ~/.config/lxsession/LXDE-pi/autostart
 sudo apt -y install python3 python3-pip scrot xdotool feh
-pip3 install --user --upgrade google-api-python-client
